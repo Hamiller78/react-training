2ae6e327e5646f4a302e675019519c28ebe0acbb
Torben Kneesch <Kneesch@gmx.de>
2024-01-22 20:31:38 +0100

    chore: make list of consultants persistent

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import AssignmentView from "./components/AssignmentView/AssignmentView";
 import ConsultantFactory from "./services/ConsultantFactory";
 import Consultant from "./entities/Consultant";
 
 function App() {
-  const consultants: Consultant[] = CreateRandomConsultants(10);
+  const [consultants, setConsultants] = useState<Consultant[]>([]);
+
+  useEffect(() => {
+    const storedConsultants = localStorage.getItem("consultants");
+    if (storedConsultants) {
+      setConsultants(JSON.parse(storedConsultants));
+    } else {
+      const newConsultants = createRandomConsultants(10);
+      setConsultants(newConsultants);
+      localStorage.setItem("consultants", JSON.stringify(newConsultants));
+    }
+  }, []);
 
   return (
     <div className="App">
@@ -16,7 +27,7 @@ function App() {
   );
 }
 
-function CreateRandomConsultants(numberOfConsultants: number): Consultant[] {
+function createRandomConsultants(numberOfConsultants: number): Consultant[] {
   const consultants: Consultant[] = [];
   for (let i = 0; i < numberOfConsultants; i++) {
     consultants.push(ConsultantFactory.createRandomConsultant());
